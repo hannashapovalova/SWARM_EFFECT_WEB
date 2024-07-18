66299d7f4f25f5f1df92784b56afdc7427857bed
Anna <hannalova1991@gmail.com>
2024-07-18 13:27:20 +0100

    Added JS to create Burger Menu

---
+++ script.js
@@ -0,0 +1,6 @@
+  // document.querySelector('.burger-menu').addEventListener('click', function() {
+  //   document.querySelector('.nav-list').classList.toggle('active');
+  // });
+  document.querySelector('.burger-menu').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('active');
+  });
